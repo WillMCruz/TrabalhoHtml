971e375016444593fcd3df522b77a9d305a62cb6
WillMCruz <willianmoraescruz@hotmail.com>
2022-10-04 16:47:52 -0300

    Novo

---
+++ Js/Script.js
@@ -0,0 +1,10 @@
+let Popup = document.getElementById("Popup");
+
+function OpenPopup()
+{
+    Popup.classList.add("OpenPopup");
+}
+function ClosePopup()
+{
+    Popup.classList.remove("OpenPopup");
+}
